da22cbabc23e0ba694da6fd345e9110ca7698e0e
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 11:24:32 -0600

    Obtain lat_long for location params input

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
+    location = params[:location]
 
+    geo_conn = Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
+      f.params[:key] = ENV['MAPQUEST-API-KEY']
+    end
+
+    geo_response = conn.get('geocoding/v1/address/') do |f|
+      f.params[:location] = location
+    end
+
+    geo_json = JSON.parse(response.body, symbolize_names: true)
+    lat_long = json[:results][0][:locations][0][:latLng]
+
+    
+
+    binding.pry
   end
 end
