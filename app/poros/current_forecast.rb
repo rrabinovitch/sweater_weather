0fb93533a7e4b2437d7cf49771297238d04db352
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 15:44:51 -0600

    Add attr_readers to CurrentForecast

--- app/poros/current_forecast.rb
+++ app/poros/current_forecast.rb
@@ -1,17 +1,21 @@
 class CurrentForecast
+  attr_reader :datetime, :temp, :description, :feels_like,
+              :humidity, :visibility, :uv_index,
+              :sunrise, :sunset
+              
   def initialize(forecast_data)
-    @city = nil
-    @state = nil
-    @country = nil
-    @current_datetime = forecast_data[:current][:dt]
-    @current_temp = forecast_data[:current][:temp]
-    @current_description = forecast_data[:current][:weather][0][:description]
-    @current_feels_like = forecast_data[:current][:feels_like]
-    @current_humidity = forecast_data[:current][:humidity]
-    @current_visibility = forecast_data[:current][:visibility] # this value is in meters, needs to be changed to miles
-    @current_uv_index = forecast_data[:current][:uvi] # does not include the 'low' description displayed in wireframe
-    @current_sunrise = forecast_data[:current][:sunrise]
-    @current_sunset = forecast_data[:current][:sunset]
-    @current_sunset = forecast_data[:current][:sunset]
+    ## these should probably go elsewhere
+    # @city = nil
+    # @state = nil
+    # @country = nil
+    @datetime = forecast_data[:current][:dt]
+    @temp = forecast_data[:current][:temp]
+    @description = forecast_data[:current][:weather][0][:description]
+    @feels_like = forecast_data[:current][:feels_like]
+    @humidity = forecast_data[:current][:humidity]
+    @visibility = forecast_data[:current][:visibility] # this value is in meters, needs to be changed to miles
+    @uv_index = forecast_data[:current][:uvi] # does not include the 'low' description displayed in wireframe
+    @sunrise = forecast_data[:current][:sunrise]
+    @sunset = forecast_data[:current][:sunset]
   end
 end
