5aaa6f5db1ae073852983d0504e6811c2f7a48be
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 21:39:53 -0600

    Move forecast retrieval from facade into service

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -11,26 +11,11 @@ class ForecastFacade
 
   def get_forecast(location)
     location = get_location(location)
-    # coordinates = @geo_service.location_coordinates(location)
-    ### return value should be coordinates to then pass as args for forecast retrieval method
 
-    # coordinates = geo_json[:results][0][:locations][0][:latLng]
-    # lat = coordinates[:lat]
-    # lon = coordinates[:lng]
-
-    @weather_service.forecast_by_coordinates(location.coordinates)
+    forecast_data = @weather_service.forecast_by_coordinates(location.coordinates)
     ### return value should be the big forecast hash that can then be serialized in ForecastsController
-    # weather_conn = Faraday.new(url: 'https://api.openweathermap.org/') do |f|
-    #   f.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
-    # end
-    #
-    # weather_response = weather_conn.get('data/2.5/onecall') do |f|
-    #   f.params[:lat] = lat
-    #   f.params[:lon] = lon
-    #   f.params[:exclude] = 'minutely,alerts'
-    #   f.params[:units] = 'imperial'
-    # end
-    #
-    # forecast_data = JSON.parse(weather_response.body, symbolize_names: true)
+
+    ### COLLECT FORECAST INFO AND THEN ORGANIZE INTO LARGE HASH?
+
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,5 +1,24 @@
 class WeatherService
   def forecast_by_coordinates(coordinates)
+    response = conn.get('data/2.5/onecall') do |f|
+      f.params[:lat] = coordinates[:lat]
+      f.params[:lon] = coordinates[:lng]
+      f.params[:exclude] = 'minutely,alerts'
+      f.params[:units] = 'imperial'
+    end
 
+    parse_json(response)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.openweathermap.org/') do |f|
+      f.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
