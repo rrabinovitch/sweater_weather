06cd18363b0c6211b538c1e9a460e70011f5352b
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 09:57:20 -0600

    Pseudo code

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::FoodieController < ApplicationController
   def show
+    start_params = params[:start]
+    end_params = params[:end]
+    search_params = params[:search]
 
+    # use start and end params as is to get directions and travel time
+    # use end params and GeoService to obtain lat/long of end params
+    # use lat/long for end params and search params via ZomatoService to obtain restaurant name and address
+    # calculate time of arrival??
+    # use lat/long for end params and time of arrival/travel time via WeatherService to access forecast at time of arrival
   end
 end
--- spec/requests/api/v1/foodie_route_request_spec.rb
+++ spec/requests/api/v1/foodie_route_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Foodie route request' do
   it 'can retrieve food and forecast information for a destination city, including:
-        - the end end city
+        - the end city
         - estimated travel time
         - the name and address of a restaurant that matches the search parameter
         - the forecast at your time of arrival' do
