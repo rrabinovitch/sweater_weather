5d38fef8def55eac929782b9a794475517156d31
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 22:37:33 -0600

    Clean up

--- app/poros/current_forecast.rb
+++ app/poros/current_forecast.rb
@@ -3,15 +3,15 @@ class CurrentForecast
               :humidity, :visibility, :uv_index,
               :sunrise, :sunset
 
-  def initialize(current_forecast_data)
-    @datetime = current_forecast_data[:dt]
-    @temp = current_forecast_data[:temp]
-    @description = current_forecast_data[:weather][0][:description]
-    @feels_like = current_forecast_data[:feels_like]
-    @humidity = current_forecast_data[:humidity]
-    @visibility = current_forecast_data[:visibility] # this value is in meters, needs to be changed to miles
-    @uv_index = current_forecast_data[:uvi] # does not include the 'low' description displayed in wireframe
-    @sunrise = current_forecast_data[:sunrise]
-    @sunset = current_forecast_data[:sunset]
+  def initialize(forecast_data)
+    @datetime = forecast_data[:dt]
+    @temp = forecast_data[:temp]
+    @description = forecast_data[:weather][0][:description]
+    @feels_like = forecast_data[:feels_like]
+    @humidity = forecast_data[:humidity]
+    @visibility = forecast_data[:visibility] # this value is in meters, needs to be changed to miles
+    @uv_index = forecast_data[:uvi] # does not include the 'low' description displayed in wireframe
+    @sunrise = forecast_data[:sunrise]
+    @sunset = forecast_data[:sunset]
   end
 end
