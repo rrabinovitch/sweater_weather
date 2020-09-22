80eb01485034f458ab2147061c00d6c68a63f43c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 11:15:05 -0600

    Refactor RouteService for SRP

--- app/services/route_service.rb
+++ app/services/route_service.rb
@@ -1,14 +1,22 @@
 class RouteService
   def get_route_data(start_params, end_params)
-    route_conn = Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
-      f.params[:key] = ENV['MAPQUEST_API_KEY']
-    end
-
-    route_response = route_conn.get('directions/v2/route') do |f|
+    response = conn.get('directions/v2/route') do |f|
       f.params[:from] = start_params
       f.params[:to] = end_params
     end
 
-    route_json = JSON.parse(route_response.body, symbolize_names: true)
+    parse_json(response)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
+      f.params[:key] = ENV['MAPQUEST_API_KEY']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
