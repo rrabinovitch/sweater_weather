a0d7205f8911aebed32c877f334e76be987d5912
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 23:39:28 -0600

    Serialize Forecast response

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,14 +1,8 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     location = params[:location]
-
     forecast = FORECAST_FACADE.get_forecast(location)
-
-    # current_forecast = CurrentForecast.new(forecast_data)
-    # hourly_forecast = HourlyForecast.new(forecast_data)
-    # daily_forecast = DailyForecast.new(forecast_data)
-
-    render json: forecast
+    render json: ForecastSerializer.new(forecast)
   end
 
   private
