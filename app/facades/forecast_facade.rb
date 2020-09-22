955ec4ac0aa4511d04efafe0215082dccc571be8
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 19:11:45 -0600

    Dream drive service implementation

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,34 @@
+class ForecastFacade
+
+  def self.get_forecast(location)
+    coordinates = GeoService.location_coordinates(location)
+    ### return value should be coordinates to then pass as args for forecast retrieval method
+    # geo_conn = Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
+    #   f.params[:key] = ENV['MAPQUEST_API_KEY']
+    # end
+    #
+    # geo_response = geo_conn.get('geocoding/v1/address/') do |f|
+    #   f.params[:location] = location
+    # end
+    #
+    # geo_json = JSON.parse(geo_response.body, symbolize_names: true)
+    # coordinates = geo_json[:results][0][:locations][0][:latLng]
+    # lat = coordinates[:lat]
+    # lon = coordinates[:lng]
+
+    WeatherService.forecast_by_coordinates(coordinates)
+    ### return value should be the big forecast hash that can then be serialized in ForecastsController
+    # weather_conn = Faraday.new(url: 'https://api.openweathermap.org/') do |f|
+    #   f.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
+    # end
+    #
+    # weather_response = weather_conn.get('data/2.5/onecall') do |f|
+    #   f.params[:lat] = lat
+    #   f.params[:lon] = lon
+    #   f.params[:exclude] = 'minutely,alerts'
+    #   f.params[:units] = 'imperial'
+    # end
+    #
+    # forecast_data = JSON.parse(weather_response.body, symbolize_names: true)
+  end
+end
