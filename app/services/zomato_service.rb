eae1bac253259cdc2e1cef9d467cbc655be487f7
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 11:29:59 -0600

    Refactor ZomatoService for SRP

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -1,16 +1,24 @@
 class ZomatoService
   def get_restaurant_data(end_coordinates, search_params)
-    zomato_conn = Faraday.new(url: 'https://developers.zomato.com/') do |f|
-      f.headers["user-key"] = ENV['ZOMATO_API_KEY']
-    end
-
-    zomato_response = zomato_conn.get('api/v2.1/search') do |f|
+    response = conn.get('api/v2.1/search') do |f|
       f.params[:q] = search_params
       f.params[:count] = 1
       f.params[:lat] = end_coordinates[:lat]
       f.params[:lon] = end_coordinates[:lng]
     end
 
-    zomato_json = JSON.parse(zomato_response.body, symbolize_names: true)
+    parse_json(response)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://developers.zomato.com/') do |f|
+      f.headers["user-key"] = ENV['ZOMATO_API_KEY']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
