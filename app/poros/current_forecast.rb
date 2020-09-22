e35d4bbd3cc2883d30b4ab104983b62f2bc7e632
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-21 22:36:21 -0600

    Refactor instantiation of CurrentForecast PORO

--- app/poros/current_forecast.rb
+++ app/poros/current_forecast.rb
@@ -2,20 +2,16 @@ class CurrentForecast
   attr_reader :datetime, :temp, :description, :feels_like,
               :humidity, :visibility, :uv_index,
               :sunrise, :sunset
-              
-  def initialize(forecast_data)
-    ## these should probably go elsewhere
-    # @city = nil
-    # @state = nil
-    # @country = nil
-    @datetime = forecast_data[:current][:dt]
-    @temp = forecast_data[:current][:temp]
-    @description = forecast_data[:current][:weather][0][:description]
-    @feels_like = forecast_data[:current][:feels_like]
-    @humidity = forecast_data[:current][:humidity]
-    @visibility = forecast_data[:current][:visibility] # this value is in meters, needs to be changed to miles
-    @uv_index = forecast_data[:current][:uvi] # does not include the 'low' description displayed in wireframe
-    @sunrise = forecast_data[:current][:sunrise]
-    @sunset = forecast_data[:current][:sunset]
+
+  def initialize(current_forecast_data)
+    @datetime = current_forecast_data[:dt]
+    @temp = current_forecast_data[:temp]
+    @description = current_forecast_data[:weather][0][:description]
+    @feels_like = current_forecast_data[:feels_like]
+    @humidity = current_forecast_data[:humidity]
+    @visibility = current_forecast_data[:visibility] # this value is in meters, needs to be changed to miles
+    @uv_index = current_forecast_data[:uvi] # does not include the 'low' description displayed in wireframe
+    @sunrise = current_forecast_data[:sunrise]
+    @sunset = current_forecast_data[:sunset]
   end
 end
