96724a71188d0230acbf6ce21c22eb18475525f0
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 10:11:57 -0600

    Access ForecastFacade

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::FoodieController < ApplicationController
     end_params = params[:end]
     search_params = params[:search]
 
+    # use start and end params as is to get directions and travel time
     route_conn = Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
       f.params[:key] = ENV['MAPQUEST_API_KEY']
     end
@@ -16,11 +17,16 @@ class Api::V1::FoodieController < ApplicationController
     route_json = JSON.parse(route_response.body, symbolize_names: true)
 
     travel_time = route_json[:route][:legs].first[:time] # in seconds
+
+
     binding.pry
-    # use start and end params as is to get directions and travel time
     # use end params and GeoService to obtain lat/long of end params
     # use lat/long for end params and search params via ZomatoService to obtain restaurant name and address
     # calculate time of arrival??
     # use lat/long for end params and time of arrival/travel time via WeatherService to access forecast at time of arrival
   end
+
+  private
+
+  FORECAST_FACADE ||= ForecastFacade.new
 end
