2ab80974170ee9a465e86023c7dbf91251ee6aff
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 10:20:52 -0600

    Establish zomato API conn & access restaurant info

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -22,9 +22,24 @@ class Api::V1::FoodieController < ApplicationController
     # use end params and GeoService to obtain lat/long of end params
     end_location = FORECAST_FACADE.get_location(end_params)
     end_coordinates = end_location.coordinates
-    
-    binding.pry
+
     # use lat/long for end params and search params via ZomatoService to obtain restaurant name and address
+    zomato_conn = Faraday.new(url: 'https://developers.zomato.com/') do |f|
+      f.headers["user-key"] = ENV['ZOMATO_API_KEY']
+    end
+
+    zomato_response = zomato_conn.get('api/v2.1/search') do |f|
+      f.params[:q] = search_params
+      f.params[:count] = 1
+      f.params[:lat] = end_coordinates[:lat]
+      f.params[:lon] = end_coordinates[:lng]
+    end
+
+    zomato_json = JSON.parse(zomato_response.body, symbolize_names: true)
+
+    restaurant_name = zomato_json[:restaurants].first[:restaurant][:name]
+    restaurant_address = zomato_json[:restaurants].first[:restaurant][:location][:address]
+    binding.pry
     # calculate time of arrival??
     # use lat/long for end params and time of arrival/travel time via WeatherService to access forecast at time of arrival
   end
