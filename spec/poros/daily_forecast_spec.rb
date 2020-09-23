3ca510846155c20ad2d51ec1523d88e01989a344
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 23:46:37 -0600

    Test various precipitation conditions for DailyForecast PORO

--- spec/poros/daily_forecast_spec.rb
+++ spec/poros/daily_forecast_spec.rb
@@ -3,19 +3,8 @@ require 'rails_helper'
 RSpec.describe DailyForecast do
   it 'has attributes' do
     data = {:dt=>1600711200,
-        :sunrise=>1600692427,
-        :sunset=>1600736311,
         :temp=>{:day=>83.07, :min=>65.21, :max=>85.33, :night=>66.33, :eve=>83.12, :morn=>65.21},
-        :feels_like=>{:day=>74.26, :night=>57.4, :eve=>74.66, :morn=>60.64},
-        :pressure=>1018,
-        :humidity=>15,
-        :dew_point=>31.14,
-        :wind_speed=>8.95,
-        :wind_deg=>107,
-        :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}],
-        :clouds=>0,
-        :pop=>0.04,
-        :uvi=>6.48}
+        :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}]}
 
     daily_forecast = DailyForecast.new(data)
 
@@ -25,5 +14,45 @@ RSpec.describe DailyForecast do
     expect(daily_forecast.precipitation).to eq(0)
     expect(daily_forecast.high_temp).to eq(85.33)
     expect(daily_forecast.low_temp).to eq(65.21)
+  end
+
+  it 'can calculate total_precipitation when no rain & no snow' do
+    data = {:dt=>1600711200,
+        :temp=>{:day=>83.07, :min=>65.21, :max=>85.33, :night=>66.33, :eve=>83.12, :morn=>65.21},
+        :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}]}
+
+    daily_forecast = DailyForecast.new(data)
+    expect(daily_forecast.precipitation).to eq(0)
+  end
+
+  it 'can calculate total_precipitation when no rain & yes snow' do
+    data = {:dt => 1600797600,
+        :temp=>{:day=>81.86, :min=>62.44, :max=>86.99, :night=>62.44, :eve=>77.85, :morn=>66.94},
+        :weather=>[{:id=>500, :main=>"Rain", :description=>"light rain", :icon=>"10d"}],
+        :snow=>2.01}
+
+    daily_forecast = DailyForecast.new(data)
+    expect(daily_forecast.precipitation).to eq(2.01)
+  end
+
+  it 'can calculate total_precipitation when yes rain & no snow' do
+    data = {:dt => 1600797600,
+        :temp=>{:day=>81.86, :min=>62.44, :max=>86.99, :night=>62.44, :eve=>77.85, :morn=>66.94},
+        :weather=>[{:id=>500, :main=>"Rain", :description=>"light rain", :icon=>"10d"}],
+        :rain=>1.58}
+
+    daily_forecast = DailyForecast.new(data)
+    expect(daily_forecast.precipitation).to eq(1.58)
+  end
+
+  it 'can calculate total_precipitation when yes rain & yes snow' do
+    data = {:dt => 1600797600,
+        :temp=>{:day=>81.86, :min=>62.44, :max=>86.99, :night=>62.44, :eve=>77.85, :morn=>66.94},
+        :weather=>[{:id=>500, :main=>"Rain", :description=>"light rain", :icon=>"10d"}],
+        :rain=>2.11,
+        :snow=>1.05}
+
+    daily_forecast = DailyForecast.new(data)
+    expect(daily_forecast.precipitation).to eq(3.16)
   end
 end
