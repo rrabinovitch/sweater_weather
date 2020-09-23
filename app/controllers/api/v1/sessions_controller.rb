618c55536451df8801bc3ef3d4aecdc1afcf8d29
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 17:19:25 -0600

    Add conditional for when user not found with submitted email

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
+    # binding.pry
     if user
       render json: UserSerializer.new(user)
+    elsif user.nil?
+      render json: { body: 'User not found' }, status: 401
     end
   end
 
--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Login request' do
-  it 'can successfully log in a user' do
+  it 'is successful and returns user data if good credentials are submitted' do
+    user = create(:user)
+    login_params = {email: user.email,
+                  password: user.password}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/sessions', params: JSON.generate(login_params), headers: headers
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    login_json = JSON.parse(response.body, symbolize_names: true)
+    expect(login_json[:data][:type]).to eq("user")
+    expect(login_json[:data][:attributes][:email]).to eq(user.email)
+    expect(login_json[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+
+  it 'is unsuccessful if email is not found in db' do
+    login_params = {email: "whatever@example.com",
+                  password: "password"}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/sessions', params: JSON.generate(login_params), headers: headers
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    no_login_json = JSON.parse(response.body, symbolize_names: true)
+    expect(no_login_json[:body]).to eq('User not found')
+  end
+
+  xit 'is unsuccessful if email and password do not match' do
     user = create(:user)
     login_params = {email: user.email,
                   password: user.password}
