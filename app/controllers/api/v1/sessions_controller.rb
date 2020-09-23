a8bb6660dca5622752e7bf8922690bd15f42dccd
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 17:03:16 -0600

    Find user and return user info

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by(email: session_params[:email])
+    if user
+      render json: UserSerializer.new(user)
+    end
+  end
 
+  private
+
+  def session_params
+    params.permit(:email, :password)
   end
 end
