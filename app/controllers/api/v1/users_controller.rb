71fd9afcb37d8c4f064496a7e2db9e950314c186
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 14:50:02 -0600

    Add API key to Users table

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    User.create(user_params)
+    if successful_password_confirmation
+      User.create(user_params)
+    end
   end
 
   private
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def successful_password_confirmation
+    user_params[:password] == user_params[:password_confirmation]
   end
 end
