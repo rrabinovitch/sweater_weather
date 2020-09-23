6bb9c89f9655305173d3fca288658062abae7c58
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 17:06:20 -0600

    Add additional expectations to successful login spec

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe 'Login request' do
     expect(response).to be_successful
     expect(response.status).to eq(200)
     expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    login_json = JSON.parse(response.body, symbolize_names: true)
+    expect(login_json[:data][:type]).to eq("user")
+    expect(login_json[:data][:attributes][:email]).to eq(user.email)
+    expect(login_json[:data][:attributes][:api_key]).to eq(user.api_key)
   end
 end
