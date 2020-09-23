b0cfd61e20dcaf47e5826adef992d0b363d652ed
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 23:07:10 -0600

    Use VCR for BackgroundFacade spec

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe BackgroundFacade do
-  before :each do
-    @background_facade = BackgroundFacade.new
-  end
-
   it '#get_location_img' do
-    location = 'denver,co'
-    image = @background_facade.get_location_img(location)
+    VCR.use_cassette('Denver image search') do
+      background_facade = BackgroundFacade.new
+      location = 'denver,co'
+      image = background_facade.get_location_img(location)
 
-    expect(image).to be_an(Image)
+      expect(image).to be_an(Image)
+    end
   end
 end
