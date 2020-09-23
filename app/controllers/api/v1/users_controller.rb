af209d6bafc66da1c2a98edd9c1f3a41de402c2b
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 00:38:35 -0600

    Define private user_params method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    binding.pry
+  end
 
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
