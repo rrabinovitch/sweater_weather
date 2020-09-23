ee820c8962f160349d0182b193e0522a5e99aa63
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-22 22:40:30 -0600

    Refactor PexelService for SRP

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -2,12 +2,12 @@ class Image
   def initialize(img_data, location)
     @id = nil
     @location = location
-    @image_url = img_data[:photos][0][:url]
+    @image_url = img_data[:url]
     @credit = credit(img_data)
   end
 
   def credit(img_data)
     { source: "pexels.com",
-      photographer: img_data[:photos][0][:photographer] }
+      photographer: img_data[:photographer] }
   end
 end
--- app/services/pexel_service.rb
+++ app/services/pexel_service.rb
@@ -1,14 +1,22 @@
 class PexelService
   def img_by_location(location)
-    conn = Faraday.new(url: 'https://api.pexels.com/') do |f|
-      f.headers['Authorization'] = ENV['PEXELS_API_KEY']
-    end
-
     response = conn.get('v1/search') do |f|
       f.params[:query] = location
       f.params[:per_page] = 1
     end
 
-    img_json = JSON.parse(response.body, symbolize_names: true)
+    parse_json(response)[:photos][0]
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.pexels.com/') do |f|
+      f.headers['Authorization'] = ENV['PEXELS_API_KEY']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
