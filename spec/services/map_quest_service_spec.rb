ed0fe38fdb3416e3b5c78249d699c4452e8c3281
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 22:25:12 -0600

    Add test for MQService::directions_data method

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe MapQuestService do
       results = MapQuestService.location_data('denver,co')
 
       expect(results).to be_a(Hash)
-      expect(results[:street]).to be_empty
       expect(results[:adminArea5]).to be_a(String)
       expect(results[:adminArea3]).to be_a(String)
       expect(results[:adminArea1]).to be_a(String)
@@ -14,5 +13,14 @@ RSpec.describe MapQuestService do
       expect(results[:latLng][:lat]).to be_a(Float)
       expect(results[:latLng][:lng]).to be_a(Float)
     end
+  end
+
+  it 'can return directions data' do
+    origin = 'Denver, CO'
+    destination = 'Pueblo, CO'
+    results = MapQuestService.directions_data(origin, destination)
+
+    expect(results).to be_a(Hash)
+    expect(results[:time]).to be_an(Integer)
   end
 end
