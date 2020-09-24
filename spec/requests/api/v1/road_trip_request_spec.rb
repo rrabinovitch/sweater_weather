f8774342e5b5e15d0c61af76079256493a659e6a
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 21:24:36 -0600

    Add sad path tests for road trip request test

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -32,4 +32,85 @@ RSpec.describe 'Road trip request' do
     expect(road_trip_json[:data][:attributes][:user_id]).to eq(road_trip.user_id)
     expect(road_trip_json[:data][:attributes]).to_not have_key(:api_key)
   end
+
+  it 'does not create a road trip and returns error if api key is not associated with actual user' do
+    user = create(:user)
+    expect(user.road_trips.count).to eq(0)
+
+    road_trip_params = {origin: "Denver, CO",
+                    destination: "Pueblo, CO",
+                    api_key: "123wrongkey321"}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/road_trip', params: JSON.generate(road_trip_params), headers: headers
+    road_trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    no_road_trip_json = JSON.parse(response.body, symbolize_names: true)
+    expect(no_login_json[:error]).to eq('Unsuccessful API key authentication')
+  end
+
+  xit 'does not create a road trip and returns error if api key not included in request body' do
+    user = create(:user)
+    expect(user.road_trips.count).to eq(0)
+
+    road_trip_params = {origin: "Denver, CO",
+                    destination: "Pueblo, CO"}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/road_trip', params: JSON.generate(road_trip_params), headers: headers
+    road_trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    no_road_trip_json = JSON.parse(response.body, symbolize_names: true)
+    expect(no_login_json[:error]).to eq('Missing API key')
+  end
+
+  xit 'does not create a road trip and returns error if origin param not included in request body' do
+    user = create(:user)
+    expect(user.road_trips.count).to eq(0)
+
+    road_trip_params = {destination: "Pueblo, CO",
+                    api_key: user.api_key}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/road_trip', params: JSON.generate(road_trip_params), headers: headers
+    road_trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    no_road_trip_json = JSON.parse(response.body, symbolize_names: true)
+    expect(no_login_json[:error]).to eq('Missing origin info')
+  end
+
+  xit 'does not create a road trip and returns error if destination param not included in request body' do
+    user = create(:user)
+    expect(user.road_trips.count).to eq(0)
+
+    road_trip_params = {origin: "Denver, CO",
+                    api_key: user.api_key}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/road_trip', params: JSON.generate(road_trip_params), headers: headers
+    road_trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    no_road_trip_json = JSON.parse(response.body, symbolize_names: true)
+    expect(no_login_json[:error]).to eq('Missing origin info')
+  end
 end
