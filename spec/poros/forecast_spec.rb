0152c1b1e13d1d2003d9747f3e946c45fbf050cc
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 22:39:59 -0600

    Fix indentation

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe Forecast do
         :adminArea1=>"US",
         :latLng=>{:lat=>39.738453, :lng=>-104.984853}}
 
-        location = Location.new(location_data)
+      location = Location.new(location_data)
 
-        forecast_data = WeatherService.forecast_by_coordinates(location.coordinates)
+      forecast_data = WeatherService.forecast_by_coordinates(location.coordinates)
 
-        forecast = Forecast.new(location, forecast_data)
+      forecast = Forecast.new(location, forecast_data)
 
-        expect(forecast).to be_a(Forecast)
-        expect(forecast.id).to eq(nil)
-        expect(forecast.location).to be_a(Location)
-        expect(forecast.current).to be_a(CurrentForecast)
-        expect(forecast.hourly.size).to eq(8)
-        expect(forecast.hourly.first).to be_an(HourlyForecast)
-        expect(forecast.daily.size).to eq(5)
-        expect(forecast.daily.first).to be_a(DailyForecast)
+      expect(forecast).to be_a(Forecast)
+      expect(forecast.id).to eq(nil)
+      expect(forecast.location).to be_a(Location)
+      expect(forecast.current).to be_a(CurrentForecast)
+      expect(forecast.hourly.size).to eq(8)
+      expect(forecast.hourly.first).to be_an(HourlyForecast)
+      expect(forecast.daily.size).to eq(5)
+      expect(forecast.daily.first).to be_a(DailyForecast)
     end
   end
 end
