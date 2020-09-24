a69fad468d1d34026d7a08e54560900a8aee5446
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 19:16:34 -0600

    Write roadtrip request spec

---
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Road trip request' do
+  it 'returns successful response with origin, destination, travel time, and general forecast info' do
+    user = create(:user)
+
+    roadtrip_params = {origin: "Denver, CO",
+                    destination: "Pueblo, CO",
+                    api_key: user.api_key}
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/road_trip', params: JSON.generate(roadtrip_params), headers: headers
+    roadtrip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+
+    roadtrip = RoadTrip.last
+
+    expect(roadtrip_json[:data][:type]).to eq("road trip")
+    expect(roadtrip_json[:data][:id]).to eq(roadtrip.id.to_s)
+    expect(roadtrip_json[:data][:attributes][:origin]).to eq(roadtrip.origin)
+    expect(roadtrip_json[:data][:attributes][:destination]).to eq(roadtrip.destination)
+    # expect(roadtrip_json[:data][:attributes][:forecast][:temp]).to eq(???)
+    # expect(roadtrip_json[:data][:attributes][:forecast][:description]).to eq(???)
+  end
+end
