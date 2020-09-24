f270b84404a64c8f2020d26f573e542adc7de20f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 21:27:49 -0600

    Change json response :body to :error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: UserSerializer.new(user), status: 201
     else
-      render json: { body: user.errors.full_messages.to_sentence }, status: 400
+      render json: { error: user.errors.full_messages.to_sentence }, status: 400
     end
   end
 
