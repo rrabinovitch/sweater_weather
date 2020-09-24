407bc5e271727ae7d4e40e3250926441f8973472
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-23 22:12:24 -0600

    Break out ::create_road_trip_for_user and ::assemble_arrival_forecast methods

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,17 +1,26 @@
 class RoadTripFacade
   def self.get_road_trip(origin, destination, user)
     directions_data = MapQuestService.directions_data(origin, destination)
-    travel_time_sec = directions_data[:time]
-    travel_time_min = (travel_time_sec.to_f / 60)
-    travel_time_hr = (travel_time_min.to_f / 60)
+    travel_time_hr = ( directions_data[:time].to_f / 360 )
+
     destination_coordinates = MapQuestService.location_data(destination)[:latLng]
 
     forecast_data = WeatherService.forecast_by_coordinates(destination_coordinates)
 
-    arrival_forecast = forecast_data[:hourly][travel_time_hr.round - 1]
-    arrival_temp = arrival_forecast[:temp]
-    arrival_description = arrival_forecast[:weather][0][:description]
-    user.road_trips.create(origin: origin, destination: destination, travel_time: travel_time_hr,
-      forecast_temp: arrival_temp, forecast_description: arrival_description)
+    arrival_forecast = assemble_arrival_forecast(forecast_data[:hourly][travel_time_hr.round - 1])
+
+    create_road_trip_for_user(origin, destination, travel_time_hr, arrival_forecast, user)
+  end
+
+  def self.create_road_trip_for_user(origin, destination, travel_time, forecast, user)
+    user.road_trips.create(origin: origin, destination: destination, travel_time: travel_time,
+      forecast_temp: forecast[:temp], forecast_description: forecast[:description])
+  end
+
+  def self.assemble_arrival_forecast(arrival_forecast_data)
+    {
+      temp: arrival_forecast_data[:temp],
+      description: arrival_forecast_data[:weather][0][:description]
+    }
   end
 end
