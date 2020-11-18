e7eef3a7d0fb18f51d0e2ae61b1472f59b8708bc
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-18 14:18:08 -0700

    Configure dd & rspec integration

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,4 +1,9 @@
+require 'rspec'
+require 'ddtrace'
+
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rails
   c.use :rails
+  # Configure default RSpec integration
+  c.use :rspec, options
 end
